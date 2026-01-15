5e61663a0e95aab915ef5123a02e5e7f3e6a4cb5
chaitu11106-coder <chaitu11106@gmail.com>
2026-01-15 21:07:48 +0530

    datatypes basics

---
+++ 2_Datatypes.js
@@ -0,0 +1,13 @@
+"use strict";//treats all JS codes as newer version
+let name="shri"
+let age=21
+//number
+//string
+//bigint
+//boolean
+let weight=null //null : where we give it as 0
+let city //undefined 
+//symbol=====used when we want unique
+//object
+console.log(typeof "shri");
+
