711c8ae84f4a3400be166c78fc59214d75814bc6
chaitu11106-coder <chaitu11106@gmail.com>
2026-01-17 16:01:48 +0530

    conversion of datatypes

---
+++ 3_Conversion.js
@@ -0,0 +1,37 @@
+let age =22
+console.log(typeof age);
+let valueInnumber=Number(age)
+console.log(valueInnumber);
+
+let isLogged="shriram"
+//--------Operations------//
+console.log(2+2);//addition
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);//it calculates 2 to the power 2
+console.log(2/2);
+console.log(2%2);
+ let str1="hello"
+ let str2=" sweety"
+ let str3=str1+str2
+ console.log(str3);
+
+ console.log(2+2)// op=4
+ console.log("1"+2)//op=12
+ console.log(2+"2")//op=22
+ console.log(2+2+"1")//op=41 ------Here the first number is in integer type so whole is converted to int
+
+
+ console.log("2"+1+2)//op=212 //here first number id string type so conversion automatally in str
+ let num1,num2,num3
+ num1=num2=num3=2+2
+ console.log(num2);
+
+ let gameCounter=100
+ gameCounter++;
+ console.log(gameCounter)
+ 
+ 
+
+
+
