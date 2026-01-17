bc65d5320c9906eaa32da2ba2243bb6888d98ed5
chaitu11106-coder <chaitu11106@gmail.com>
2026-01-17 17:07:56 +0530

    stack and heap

---
+++ memory.js
@@ -0,0 +1,14 @@
+//STACK : PRIMITIVE DATA TYPE USES STACK MEMORY
+//HEAP : NON PRIMITIVE  DATA TYPE USESHEAP MEMORY
+
+//IN STACKK WE GET REPETETIVE VALUES 
+
+//IN HEAP WE GET REFERENCE VALUES
+
+let youtube="youtubename"
+youtube="chaiaurrcode"
+
+console.log(youtube);
+
+
+//displays chai aufr code as output 
