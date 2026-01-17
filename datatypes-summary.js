dd2ebc4a8814a7daf95c38cb2bc223b3ad8d0179
chaitu11106-coder <chaitu11106@gmail.com>
2026-01-17 16:24:07 +0530

    datatypes summary

---
+++ datatypes-summary.js
@@ -0,0 +1,25 @@
+// PRIMITIVE DATA TYPES::
+//7 types: Strings ,
+// number,
+// boolean,
+// null:empty , 
+// undefined:declared var but no value , 
+// symbol:to make a value unique
+//Bigint 
+
+//JAVASCRIPT IS A DYNAMICALLY TYPES LANGUAGE
+
+//REFERENCE TYPES : NO PRIMITIVE 
+//Arrays
+//Objects
+//Functions 
+let Bignumber=237462837468234n;
+
+const heros=["shaktimaan","spiderman","padman"];//example of array
+
+let myobj={   //ex of object
+    name:"shri", 
+    age :22,
+}
+
+console.log(typeof Bignumber);//example of function
