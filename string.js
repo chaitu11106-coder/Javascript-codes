314c080f994485cf3a3a878acc61cd108c478a40
chaitu11106-coder <chaitu11106@gmail.com>
2026-01-17 17:53:36 +0530

    strings and its methods

---
+++ string.js
@@ -0,0 +1,15 @@
+const name="shri"
+const repocount=52
+console.log(`hello my name is ${name} and my repo count is ${repocount} `); // way to write 
+
+const gamenew=new String('shriram');
+console.log(gamenew.length);
+console.log(gamenew.toUpperCase());
+console.log(gamenew.charAt(3));//position of index char
+console.log(gamenew.substr(0,4));// takes a part of string 
+console.log(gamenew.slice(-5,4));//takes from reverse 
+console.log(gamenew.trim())//cuts the extra space before or after the string
+
+//READ DOCUMENTATION AND FIND ALL METHODS -----HW
+
+
