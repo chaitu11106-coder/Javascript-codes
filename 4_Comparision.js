9a95738b44dc2df0d4ce461893b5851537f8e347
chaitu11106-coder <chaitu11106@gmail.com>
2026-01-17 16:09:54 +0530

    comparision of operators

---
+++ 4_Comparision.js
@@ -0,0 +1,15 @@
+console.log(2>1);//true
+console.log(2>=1);//true
+console.log(2<1);//false
+console.log(2==1);//false
+console.log(2!=1);//true
+
+////=================================================
+
+console.log(null>0);//f
+console.log(null<0);//f
+console.log(null==0);//f
+
+console.log("2"===2);// === triple equals to is KNOWN AS STRICT CHECK9
+
+
