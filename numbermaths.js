1891f08b6fbc09b99803e9caf95b48dc2e75fae4
chaitu11106-coder <chaitu11106@gmail.com>
2026-01-19 22:40:42 +0530

    numbers and maths

---
+++ numbermaths.js
@@ -0,0 +1,9 @@
+const balance=100.00
+console.log(typeof balance)
+console.log(balance.toExponential(2));
+console.log(balance.toFixed(2));
+console.log(balance.toPrecision(2));
+
+const hunderedsssss=1000000
+console.log(typeof hunderedsssss)
+console.log(hunderedsssss.toLocaleString('en-IN'));
