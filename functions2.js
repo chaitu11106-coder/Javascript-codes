2b2dc94498e44f2bd59f8eb52de78d0640122caf
chaitu11106-coder <chaitu11106@gmail.com>
2026-01-21 19:41:22 +0530

    function and objects

---
+++ functions2.js
@@ -0,0 +1,13 @@
+function calulcatecartprice(...num1){
+    return num1
+}
+console.log(calulcatecartprice(200,200,600));
+////__________________________________________----------------------
+const user={
+    name:"shri",
+    age:21
+}
+
+function handleobject(anyobject){
+    console.log(`any :name is ${anyobject.username}`);
+}
