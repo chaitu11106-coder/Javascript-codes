3be546d50e5c9c7a0f8fa964b034586eafb70054
chaitu11106-coder <chaitu11106@gmail.com>
2026-01-21 18:10:10 +0530

    objects destructring and json API

---
+++ objects2.js
@@ -0,0 +1,12 @@
+//Objects destructing 
+const course ={
+    coursename:"js in hindi",
+    price:99,
+    courseInstructor:"hitesh"
+}
+
+//courseinstructor
+const {courseInstructor: instructor}=course;
+//console.log(courseInstructor);
+
+//console.log(instructor);
