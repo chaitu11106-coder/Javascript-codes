1361f0feef99de4ec53e8210a19649e69882486c
chaitu11106-coder <chaitu11106@gmail.com>
2026-04-03 20:32:19 +0530

    operators

---
+++ operatorgyan.js
@@ -0,0 +1,25 @@
+//assignment operator
+let x=10;
+let y;
+y=x;
+console.log(y);
+
+//arithmetic operator
+let a=10;
+b=a++;
+a--;
+console.log(b);
+
+//logical operator
+const happy =true ||false;
+console.log(happy);
+
+//conditional op
+// === strict equals to
+console.log(333===333);
+
+//comparision op
+console.log(120>12);
+console.log(120>=12);
+console.log(120<12);
+
---
+++ vargyan.js
@@ -0,0 +1,10 @@
+//variables in js
+let language ='Javascript';
+console.log(language);
+
+//const
+const x=10;
+console.log(x);
+
+//function scope
+var lang='Python';
