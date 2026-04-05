6594bca617ef30da034b750e18b2e5dcb83c6063
chaitu11106-coder <chaitu11106@gmail.com>
2026-04-05 09:14:42 +0530

    arrays and methods

---
+++ arraygyan.js
@@ -0,0 +1,20 @@
+const languages=['Java','c++','py'];
+console.log(languages);
+console.log(languages.length);
+languages.push('ruby');
+console.log(languages);
+
+//objects
+const actors=[
+    {
+        fname:'actor1',
+        pay:100,
+    },
+    {
+        fname:'actor2',
+        pay:200,
+    },
+];
+console.log(actors);
+
+//
